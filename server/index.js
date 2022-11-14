ae7193c2bb1f0409d0c2efc6439a0158833ea983
nokeludak <ksmajic996@hotmail.com>
2022-11-13 16:30:18 -0800

    refresh

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const EmployeeRoute = require("./routes/employeeRoute")
 const GetAllEmployees = require("./routes/employeeRoute")
 const getEmployeesById = require("./routes/employeeRoute")
+const path = require("path")
 
 
 require("dotenv").config()
@@ -15,12 +16,24 @@ const app = express();
 app.use(cors())
 
 app.use(express.json())
-app.get("/", (req,res)=>{
-    res.json("server start")
-})
+
 
 app.use('/', GetAllEmployees)
 app.use("/", getEmployeesById)
+
+__dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,"client/build")))
+
+    app.get("*", ( req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build","index.html"))
+    })
+}else{
+    app.get("/", (req,res)=>{
+        res.send("server start")
+    })
+}
+
 
 app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running on ${process.env.PORT}`)
